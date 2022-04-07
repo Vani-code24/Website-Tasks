cb23ffae25386d32f8911634d18ab0a11df7289e
Vani-code24 <70517782+Vani-code24@users.noreply.github.com>
2022-04-07 18:48:04 +0530

    Delete Task3 directory

--- Task3/Assets/script.js
+++
@@ -1,6 +0,0 @@
-const ham = document.querySelector(".hamburger"); 
-const navbar= document.querySelector(".nav__list"); 
-ham.addEventListener('click',()=>{
-    ham.classList.toggle("change")
-    navbar.classList.toggle("nav-change")
-})
